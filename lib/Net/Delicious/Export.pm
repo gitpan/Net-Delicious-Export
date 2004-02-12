027b62d92e4bdae9db6a5454aeab9933976d518f
Aaron Straup Cope <ascope@cpan.org>
2004-02-12 14:00:11 +0000

    Import of ASCOPE/Net-Delicious-Export-1.1 from CPAN.
    gitpan-cpan-distribution: Net-Delicious-Export
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         ASCOPE/Net-Delicious-Export-1.1.tar.gz
    gitpan-cpan-author:       ASCOPE
    gitpan-cpan-maturity:     released

--- lib/Net/Delicious/Export.pm
+++ lib/Net/Delicious/Export.pm
@@ -3,7 +3,7 @@ use strict;
 package Net::Delicious::Export;
 use base qw (XML::SAX::Base);
 
-# $Id: Export.pm,v 1.2 2004/02/09 19:42:28 asc Exp $
+# $Id: Export.pm,v 1.3 2004/02/12 13:44:46 asc Exp $
 
 =head1 NAME
 
@@ -21,7 +21,7 @@ This package subclasses I<XML::SAX::Base>.
 
 =cut
 
-$Net::Delicious::Export::VERSION = '1.0';
+$Net::Delicious::Export::VERSION = '1.1';
 
 =head1 PACKAGE METHODS
 
@@ -43,11 +43,11 @@ sub new {
 
 =head1 VERSION
 
-1.0
+1.1
 
 =head1 DATE
 
-$Date: 2004/02/09 19:42:28 $
+$Date: 2004/02/12 13:44:46 $
 
 =head1 AUTHOR
 
--- t/01-load.t
+++ t/01-load.t
@@ -1,5 +1,7 @@
 use strict;
 use Test::More;
 
-plan tests => 1;
+plan tests => 2;
+
 use_ok(qq(Net::Delicious::Export));
+use_ok(qq(Net::Delicious::Export::Post));
